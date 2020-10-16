5b35a3c6508e3b8cf2c3575aa80cd219c6fc3a00
Hannah Gunn <hannahgunn97@gmail.com>
2020-10-16 14:59:31 +0100

    🏗  Added ID routes to delete, update and view

--- server/routes/timer.js
+++ server/routes/timer.js
@@ -25,4 +25,31 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
+router.route('/:id').get((req, res) => {
+    Timer.findById(req.params.id)
+        .then(times => res.json(times))
+        .catch(err => res.status(400).json('Error: '+ err))
+})
+
+router.route('/:id').delete((req, res) => {
+    Timer.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Time deleted'))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/update/:id').post((req, res) => {
+    Timer.findById(req.params.id)
+        .then(times => {
+            times.time = Number(req.body.time),
+            times.title = req.body.title,
+            times.description = req.body.description
+
+            times.save()
+            .then(() => res.json('Time updated'))
+            .catch(err => res.status(400).json('Error: ' + err))
+        })
+
+        .catch(err => res.status(400).json('Error: '+ err))
+})
+
 module.exports = router
